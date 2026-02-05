20a5ae8780ced0a6d9d0055695fff6de6656aa9d
Ramed33 <deseca333@gmail.com>
2026-02-05 10:02:15 -0600

    MySQL

--- configuration/settings.py
+++ configuration/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'configuration.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangotest',
+        'USER': 'root',
+        'PASSWORD': '123456789',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
