6cf803b8292474faf6dd7a6a8ac8ee28be0b59e3
Ramed33 <deseca333@gmail.com>
2026-02-06 20:20:23 -0600

    Cors allowed

--- configuration/settings.py
+++ configuration/settings.py
@@ -33,7 +33,7 @@ CSRF_TRUSTED_ORIGINS = ['https://dsc-ideas.cloud']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://dsc-ideas.cloud",
+    "https://dsc-ideas.cloud",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
